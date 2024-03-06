d7dbd21111166326333f675bed92a4cb9eed77e0
Rohit-koundal <rkshekhavat@gmail.com>
2024-03-06 23:14:22 +0530

    latest one

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import "zone.js";
+import "zone.js/dist/zone";
 platformBrowserDynamic()
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
